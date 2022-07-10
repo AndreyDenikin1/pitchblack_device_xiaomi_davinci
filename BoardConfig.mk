a62c6ec72a45746d95ec3d22dce93b1321823b58
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-07-10 10:41:25 +0300

    Add MAINTAINER

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,7 @@ TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_EXCLUDE_TWRPAPP := true
+MAINTAINER=AndreyDenikin1
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
