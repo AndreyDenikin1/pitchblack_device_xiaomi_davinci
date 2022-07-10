a469709e263abd5b3767654ffe3ccea1d64c9880
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-07-10 10:41:54 +0300

    Add PB_SCREEN_H=2340

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,7 @@ TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_EXCLUDE_TWRPAPP := true
 MAINTAINER=AndreyDenikin1
+PB_SCREEN_H=2340
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
